f30d82f9f71b8713e1d3530997c21721fb2bde32
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-10-10 11:22:54 +0200

    Changed to shorter version, includes instead of startsWith and endsWith

--- server/utils/filterBuilder.ts
+++ server/utils/filterBuilder.ts
@@ -3,9 +3,8 @@ export default function filterBuilder(filter: object) {
   let values: string[] = [];
   // Add filters dynamically to query and values array
   Object.entries(filter).forEach(([key, value]) => {
-    const endsWith =
-      value.startsWith("%") || value.endsWith("%") ? " LIKE" : " =";
-    params.push(`${key}${endsWith} ?`);
+    const endsWith = value.includes("%") ? " LIKE" : " =";
+    params.push(`${key} ${endsWith} ?`);
     console.log(value);
 
     values.push(value as string);

648ae66f75618600cc6521773133d965a38353c2
Hassecodes <hasan33aziz@hotmail.com>
2025-10-22 12:31:20 +0200

    bytte branch och la innehållet här då jag fick för många problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import HomePage from "./pages/HomePage";
 import BottomNavbar from "./components/BottomNavbar";
 import AllMoviesPage from "./pages/AllMoviesPage";
 import MovieDetailPage from "./pages/MovieDetailPage";
+import Navdesk from "./components/NavDesk";
 
 function App() {
   return (
     <>
+    <Navdesk />
       {/* <MovieDetailPage /> */}
       <AllMoviesPage />
       {/* <HomePage />
---
+++ src/components/NavDesk.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { Film, CupHot, InfoCircle, House, Person } from "react-bootstrap-icons";
+import "./NavDesk.scss";
+import logga from "../assets/NeoCinema.png";
+
+const NavDesk: React.FC = () => {
+  return (
+    <Navbar className="neo-navbar" sticky="top">
+      <Container fluid className="neo-shell">
+        {/* Vänster: logga → hem */}
+        <div className="neo-logo">
+          <NavLink to="/" className="neo-logo-link">
+            <img src={logga} alt="NeoCinema" className="neo-logo-img" />
+          </NavLink>
+        </div>
+
+        {/* Mitten: meny */}
+        <Nav className="neo-menu">
+          <NavLink to="/filmer" className="neo-link">
+            <Film className="neo-icon" />
+            {/* Filmer */}
+            <span className="neo-text">Filmer</span>
+          </NavLink>
+          <NavLink to="/kiosk" className="neo-link">
+            <CupHot className="neo-icon" />
+            {/* Kiosk */}
+            <span className="neo-text">Kiosk</span>
+          </NavLink>
+          <NavLink to="/om-oss" className="neo-link">
+            <InfoCircle className="neo-icon" />
+            {/* Om Oss */}
+            <span className="neo-text">Om oss</span>
+          </NavLink>
+          <NavLink to="/" end className="neo-link">
+            <House className="neo-icon" />
+            {/* Hem */}
+            <span className="neo-text">Hem</span>
+          </NavLink>
+        </Nav>
+
+        {/* Konto */}
+        <div className="neo-account">
+          <NavLink to="/konto" className="neo-link">
+            <Person className="neo-icon" />
+            <span className="neo-text">Konto</span>
+          </NavLink>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavDesk;

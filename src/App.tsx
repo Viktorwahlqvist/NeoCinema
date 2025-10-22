a4d691d84b43d6fca78f7631c6c1bd3e072bfb10
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-10-22 11:05:26 +0200

    initial state MoveDetailPage with useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import HomePage from "./pages/HomePage";
 import BottomNavbar from "./components/BottomNavbar";
 import AllMoviesPage from "./pages/AllMoviesPage";
+import MovieDetailPage from "./pages/MovieDetailPage";
 
 function App() {
   return (
     <>
+      {/* <MovieDetailPage /> */}
       <AllMoviesPage />
       {/* <HomePage />
       <BottomNavbar /> */}

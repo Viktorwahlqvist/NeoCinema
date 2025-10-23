593a7f21dbd96aab6cb59d0420092b7d55b217de
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-10-23 10:23:44 +0200

    updatin branch to latest update

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import HomePage from "./pages/HomePage";
 import AllMoviesPage from "./pages/AllMoviesPage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import BottomNavbar from "./components/BottomNavbar";
-import BookingPage from "./pages/BookingPage"
+// import BookingPage from "../pages/BookingPage";
 import NavDesk from "./components/NavDesk";
 import { useIsMobile } from "./hooks/useIsMobile";
 
 function App() {
-  const isMobile = useIsMobile(); 
+  const isMobile = useIsMobile();
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<AllMoviesPage />} />
-          <Route path="/movie/:id" element={<MovieDetailPage />} />
-          <Route path="/booking/:screeningId" element={<BookingPage />} />
-        </Routes>
+    // <Router>
+    //   <div className="app-container">
+    //     <Routes>
+    //       <Route path="/" element={<HomePage />} />
+    //       <Route path="/movies" element={<AllMoviesPage />} />
+    //       <Route path="/movie/:id" element={<MovieDetailPage />} />
+    //       <Route path="/booking/:screeningId" element={<BookingPage />} />
+    //     </Routes>
 
-        
-        {isMobile ? <BottomNavbar /> : <NavDesk />}
-      </div>
-    </Router>
+    //     {isMobile ? <BottomNavbar /> : <NavDesk />}
+    //   </div>
+    // </Router>
+    <MovieDetailPage />
   );
 }
 

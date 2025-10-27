55e0591a197c0a2c48734c14dc1da2111c259761
Hassecodes <hasan33aziz@hotmail.com>
2025-10-27 14:18:30 +0100

    testing
    håller på med att testa ifall navigering mellan länkarna funkar i nav

--- src/components/NavDesk.test.tsx
+++ src/components/NavDesk.test.tsx
@@ -1,7 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { describe, it, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import NavDesk from './NavDesk';
+
+//Pages to verify navigation
+function Home() {return <h1>Hem</h1>}
+function Movies() {return <h1>Filmer</h1>}
+function Kiosk() {return <h1>Kiosk</h1>}
+function AboutUs() {return <h1>Om oss</h1>}
+function Account() {return <h1>Konto</h1>}
 
 describe('NavDesk', () => {
   it('shows all the links', () => {
@@ -17,4 +25,23 @@ describe('NavDesk', () => {
     expect(screen.getByText('Hem')).toBeInTheDocument();
     expect(screen.getByText('Konto')).toBeInTheDocument();
   });
+
+  it('navigate to the right page when clicking on the link'), async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <NavDesk />
+        <Routes>
+          <Route path="/" element={Home />} />
+          <Route path="/Movies" element={Movies />} />
+          <Route path="/kiosk" element={Kiosk />} />
+          <Route path="/AboutUs" element={AboutUs />} />
+          <Route path="/konto" element={Account />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    await user.click(screen.getByRole('link', { name: 'Filmer' }));
+    expect(screen.getByRole('heading', { name: 'Filmer' })).toBeInTheDocument();
+  }
 });

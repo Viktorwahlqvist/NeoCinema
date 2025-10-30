7f8ce05d73f65b4d88d3c07e5a660c8ddaad1b71
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-10-30 13:38:17 +0100

    moved kiosk styling to correct folder.

--- src/pages/KioskPage.tsx
+++ src/pages/KioskPage.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import "./KioskPage.scss";
+import "./PagesStyle/KioskPage.scss";
 import drink from "../assets/drink2.svg";
 import popcorn from "../assets/popcorn3.svg";
 import candy from "../assets/candy2.svg";
-
-
-
 
 export default function KioskPage() {
   const hours = [
@@ -42,28 +39,48 @@ export default function KioskPage() {
           <h2 className="kioskA-heading">Välkommen till Neon Snacks!</h2>
 
           <p className="kioskA-lead">
-            Vi är stolta över att presentera vår helt nyöppnade kiosk – fullproppad med godsaker som gör
-            bioupplevelsen ännu bättre. Här hittar du allt från klassiska popcorn och läsk till färgstarka
-            snacks och sötsaker i sann retro-anda. Perfekt för dig som vill krydda filmkvällen med något extra!
+            Vi är stolta över att presentera vår helt nyöppnade kiosk –
+            fullproppad med godsaker som gör bioupplevelsen ännu bättre. Här
+            hittar du allt från klassiska popcorn och läsk till färgstarka
+            snacks och sötsaker i sann retro-anda. Perfekt för dig som vill
+            krydda filmkvällen med något extra!
           </p>
 
           <div className="kioskA-cols">
             <p>
-              Hos oss hittar du både klassiker och nya favoriter – popcorn, läsk, godis och snacks som
-              förhöjer filmupplevelsen.
+              Hos oss hittar du både klassiker och nya favoriter – popcorn,
+              läsk, godis och snacks som förhöjer filmupplevelsen.
             </p>
             <p>
-              På Neon Snacks hittar du inte bara godis och popcorn – vi serverar även enklare mat som korv med
-              bröd och andra snabba rätter. Perfekt om du vill stilla hungern innan filmen börjar!
+              På Neon Snacks hittar du inte bara godis och popcorn – vi serverar
+              även enklare mat som korv med bröd och andra snabba rätter.
+              Perfekt om du vill stilla hungern innan filmen börjar!
             </p>
           </div>
 
-           <div className="kioskA-icons">
-  <img src={drink} alt="Drink" className="kA-icon" width={80} height={80} />
-  <img src={popcorn} alt="Popcorn" className="kA-icon" width={80} height={80} />
-  <img src={candy} alt="Candy" className="kA-icon" width={80} height={80} />
-</div>
-
+          <div className="kioskA-icons">
+            <img
+              src={drink}
+              alt="Drink"
+              className="kA-icon"
+              width={80}
+              height={80}
+            />
+            <img
+              src={popcorn}
+              alt="Popcorn"
+              className="kA-icon"
+              width={80}
+              height={80}
+            />
+            <img
+              src={candy}
+              alt="Candy"
+              className="kA-icon"
+              width={80}
+              height={80}
+            />
+          </div>
         </section>
 
         {/* Högerkolumn */}
@@ -71,7 +88,7 @@ export default function KioskPage() {
           <section className="kA-box">
             <h3 className="kA-boxTitle">Öppettider</h3>
             <ul className="kA-hours">
-              {hours.map(h => (
+              {hours.map((h) => (
                 <li key={h.label}>
                   <span>{h.label}</span>
                   <span>{h.time}</span>
@@ -82,10 +99,19 @@ export default function KioskPage() {
 
           <section className="kA-box">
             <div className="kA-menuHead">
-              <img src={popcorn} alt="" aria-hidden className="kA-icon kA-icon--small" />
+              <img
+                src={popcorn}
+                alt=""
+                aria-hidden
+                className="kA-icon kA-icon--small"
+              />
               <h3>Utbud</h3>
-              <img src={drink} alt="" aria-hidden className="kA-icon kA-icon--small" />
-
+              <img
+                src={drink}
+                alt=""
+                aria-hidden
+                className="kA-icon kA-icon--small"
+              />
             </div>
             <ul className="kA-menu">
               {menu.map((m, i) =>

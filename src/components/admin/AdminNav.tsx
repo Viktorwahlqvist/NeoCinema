477b5c202b99f0c1484fad39234343a9a4c25ef5
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-11-20 11:06:41 +0100

    updated navbar etc..

--- src/components/admin/AdminNav.tsx
+++ src/components/admin/AdminNav.tsx
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 
 export default function AdminNav() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark" className='m-0'>
-      <Container className='m-0'>
-        <Link to="dashboard"><img className='admin-logo me-5' src="/NeoCinema.png" alt="NeoCinema" /></Link>
-        <Nav className="me-auto  gap-4">
-          <Link to="dashboard">Dashboard</Link>
-          <Link to="bookings">Bokningar</Link>
-          <Link to="users">Användare</Link>
-        </Nav>
+    <Navbar bg="dark" data-bs-theme="dark" expand="md" className="m-0 z-10">
+      <Container className="m-0">
+        <Link to="/">
+          <img className="admin-logo me-3" src="/NeoCinema.png" alt="NeoCinema" />
+        </Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="ms-3 gap-3">
+            <Link className="nav-link" to="bookings">Bokningar</Link>
+            <Link className="nav-link" to="users">Användare</Link>
+            <Link className="nav-link" to="/profile">Tillbaka till profil</Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
